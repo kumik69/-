23d70eb689d269e3e2ff11919e19db8d7b8b6109
kumik69 <pkot260@gmail.com>
2025-01-06 15:16:42 +0400

    Add files via upload
    2.0
    Добавлены базовые операторы

--- кальк.py
+++ кальк.py
@@ -1,3 +1,11 @@
 x1 = int(input("Введите первое число "))
 x2 = int(input("Введите второе число "))
 e1 = input("Введите оператор ")
+if e1 == "+":
+    print(x1 + x2)
+elif e1 == "-":
+    print(x1-x2)
+elif e1 == "*":
+    print(x1*x2)
+elif e1 == "/":
+    print(x1/x2)
